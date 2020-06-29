e9f3e2549edf09e6f5bc561cb53a9b92e35014b1
Kamil Zalwert <kamilz89@gmail.com>
2020-06-29 16:39:19 +0200

    22.1 - Gotowe do CodeReview

--- src/main/java/pl/zalwi/UsersController.java
+++ src/main/java/pl/zalwi/UsersController.java
@@ -2,14 +2,42 @@ package pl.zalwi;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.zalwi.data.User;
+import pl.zalwi.data.UserRepository;
 
 @Controller
 public class UsersController {
 
-    @ResponseBody
+    UserRepository userRepository;
+
+    public UsersController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @RequestMapping("/")
-    public String index(){
-        return "Strona startowa";
+    public String addUserForm(){
+        return "redirect:/index.html";
+    }
+
+    @ResponseBody
+    @RequestMapping("/users")
+    public String listOfUsers(){
+        String result = "<b>Lista użytkowników</b><br>";
+        for(User user: userRepository.getAll()){
+            result += user + "<br>";
+        }
+        result += "Powrót do <a href='/'>strony głównej</a>";
+        return result;
+    }
+
+    @RequestMapping("/add")
+    public String addUser(@RequestParam String firstName, @RequestParam String lastName, @RequestParam int age){
+        if(firstName.length()<1){
+            return "redirect:/err.html";
+        }
+        userRepository.addUser(new User(firstName, lastName, age));
+        return "redirect:/success.html";
     }
 }
---
+++ src/main/java/pl/zalwi/data/User.java
@@ -0,0 +1,22 @@
+package pl.zalwi.data;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
